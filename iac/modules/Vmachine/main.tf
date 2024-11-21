dd8ace3f0fb7ec64368c13fee42daa7dc5d5f018
ValentinoSanchez00 <valentinosanchezraverta@gmail.com>
2024-11-21 09:25:43 +0100

    intentando crear postgre

--- iac/modules/Vmachine/main.tf
+++ iac/modules/Vmachine/main.tf
@@ -29,3 +29,41 @@ resource "azurerm_virtual_machine" "virtual_machine" {
   }
 
 }
+
+
+resource "azurerm_virtual_machine_extension" "custom_script" {
+  name                 = "script-runner"
+  virtual_machine_id   = azurerm_virtual_machine.virtual_machine[1].id
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.0"
+
+  settings = <<SETTINGS
+    {
+        # Crear una carpeta para el runner y entrar en ella
+        mkdir actions-runner
+        cd actions-runner
+
+        # Descargar el paquete del runner más reciente
+        Invoke-WebRequest -Uri https://github.com/actions/runner/releases/download/v2.320.0/actions-runner-win-x64-2.320.0.zip -OutFile actions-runner-win-x64-2.320.0.zip
+
+        # (Opcional) Validar el hash
+        if ((Get-FileHash -Path actions-runner-win-x64-2.320.0.zip -Algorithm SHA256).Hash.ToUpper() -ne '9eb133e8cb25e8319f1cbef3578c9ec5428a7af7c6ec0202ba6f9a9fddf663c0'.ToUpper()) { 
+            throw 'Computed checksum did not match' 
+        }
+
+        # Extraer el instalador
+        Add-Type -AssemblyName System.IO.Compression.FileSystem
+        [System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD/actions-runner-win-x64-2.320.0.zip", "$PWD")
+
+        # Definir el token como variable
+
+        # Configurar el runner de forma automatizada (sin interacción)
+        ./config.cmd --url https://github.com/stemdo-labs/final-project-exercise-ValentinoSanchez00 --token ${{ secrets.RUNNER_TOKEN }} --name "runner-name" --work "_work" --replace
+
+        # Ejecutar el runner automáticamente
+        ./run.cmd --once
+
+    }
+  SETTINGS
+}
