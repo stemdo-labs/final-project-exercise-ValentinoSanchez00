ba2013fa07a4c0b586d6237c8106159888678ade
ValentinoSanchez00 <valentinosanchezraverta@gmail.com>
2024-11-21 09:47:11 +0100

    probando

--- iac/modules/Vmachine/variables.tf
+++ iac/modules/Vmachine/variables.tf
@@ -7,8 +7,3 @@ variable "rg_group" {
 variable "network_interface_ids" {
   
 }
-variable "runner_token" {
-  description = "Token para configurar el runner"
-  type        = string
-  sensitive   = true
-}
